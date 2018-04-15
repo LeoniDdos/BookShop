a07d06bf2a7b967099edc2f6d40e1f7277f1b409
LeoniDdos <leonid-009@mail.ru>
2018-04-15 23:18:54 +0300

    добавлены основные формы и переход по ним

--- app/src/main/java/com/libertystudio/bookshop_kursovaya/MainActivity.java
+++ app/src/main/java/com/libertystudio/bookshop_kursovaya/MainActivity.java
@@ -1,15 +1,27 @@
 package com.libertystudio.bookshop_kursovaya;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.GridView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.MenuItem;
+
+import com.libertystudio.bookshop_kursovaya.fragment.FBasket;
+import com.libertystudio.bookshop_kursovaya.fragment.FBooks;
+import com.libertystudio.bookshop_kursovaya.fragment.FSearch;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView menuBottom;
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    final Fragment fragmentBooks = new FBooks();
+    final Fragment fragmentSearch = new FSearch();
+    final Fragment fragmentBasket = new FBasket();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +30,32 @@ public class MainActivity extends AppCompatActivity {
 
         menuBottom = findViewById(R.id.navigation);
         menuBottom.setSelectedItemId(R.id.action_home);
+
+        menuBottom.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        try {
+                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                            switch (item.getItemId()) {
+                                case R.id.action_home:
+                                    fragmentTransaction.replace(R.id.content, fragmentBooks).commit();
+                                    setTitle("Книги");
+                                    return true;
+                                case R.id.action_search:
+                                    fragmentTransaction.replace(R.id.content, fragmentSearch).commit();
+                                    setTitle("Поиск");
+                                    return true;
+                                case R.id.action_basket:
+                                    fragmentTransaction.replace(R.id.content, fragmentBasket).commit();
+                                    setTitle("Корзина");
+                                    return true;
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        return false;
+                    }
+                });
     }
 }
--- app/src/main/java/com/libertystudio/bookshop_kursovaya/fragment/FBooks.java
+++ app/src/main/java/com/libertystudio/bookshop_kursovaya/fragment/FBooks.java
@@ -1,4 +1,4 @@
-package com.libertystudio.bookshop_kursovaya;
+package com.libertystudio.bookshop_kursovaya.fragment;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.libertystudio.bookshop_kursovaya.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Books.OnFragmentInteractionListener} interface
+ * {@link FBooks.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Books#newInstance} factory method to
+ * Use the {@link FBooks#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Books extends Fragment {
+public class FBooks extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +31,7 @@ public class Books extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Books() {
+    public FBooks() {
         // Required empty public constructor
     }
 
@@ -39,11 +41,11 @@ public class Books extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Books.
+     * @return A new instance of fragment FBooks.
      */
     // TODO: Rename and change types and number of parameters
-    public static Books newInstance(String param1, String param2) {
-        Books fragment = new Books();
+    public static FBooks newInstance(String param1, String param2) {
+        FBooks fragment = new FBooks();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -77,12 +79,13 @@ public class Books extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        //Commented
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
     }
 
     @Override

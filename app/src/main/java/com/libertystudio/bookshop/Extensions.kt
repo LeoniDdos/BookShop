ae487b1378a2a97bfce6a354a0b9a26194f1349f
LeoniDdos <leonid-009@mail.ru>
2020-05-16 14:37:33 +0300

    add extension showImage; Glide; placeholder; refactoring item_book

--- app/src/main/java/com/libertystudio/bookshop/Extensions.kt
+++ app/src/main/java/com/libertystudio/bookshop/Extensions.kt
@@ -4,7 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 fun Context.inflate(resource: Int, root: ViewGroup, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(this).inflate(resource, root, attachToRoot)
+}
+
+fun ImageView.showImage(url: String, placeholder: Int = R.drawable.placeholder_book) {
+    Glide
+            .with(this)
+            .load(url)
+            .placeholder(placeholder)
+            .into(this)
 }
--- app/src/main/java/com/libertystudio/bookshop/adapter/BookAdapter.kt
+++ app/src/main/java/com/libertystudio/bookshop/adapter/BookAdapter.kt
@@ -2,10 +2,12 @@ package com.libertystudio.bookshop.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.libertystudio.bookshop.R
 import com.libertystudio.bookshop.entity.Book
 import com.libertystudio.bookshop.inflate
+import com.libertystudio.bookshop.showImage
 
 class BookAdapter : BaseAdapter<Book>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
@@ -14,10 +16,12 @@ class BookAdapter : BaseAdapter<Book>() {
     }
 
     inner class BookViewHolder(itemView: View) : BaseViewHolder<Book>(itemView) {
-        private val tvTitle = itemView.findViewById<TextView>(R.id.textViewTitle)
-        private val tvPrice = itemView.findViewById<TextView>(R.id.textViewPrice)
+        private val ivImage = itemView.findViewById<ImageView>(R.id.ivBookImage)
+        private val tvTitle = itemView.findViewById<TextView>(R.id.tvBookTitle)
+        private val tvPrice = itemView.findViewById<TextView>(R.id.tvBookPrice)
 
         override fun bind(item: Book) {
+            ivImage.showImage(url = "https://cdn1.ozone.ru/multimedia/1022150114.jpg")
             tvTitle.text = item.title + " (" + item.author.surname + " " + item.author.name.substring(0, 1) + ".)"
             tvPrice.text = "${item.price} руб."
 
